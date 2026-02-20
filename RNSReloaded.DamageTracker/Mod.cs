b2c21f81008c4ed0c2d083bf68dfe85ef1cf4999
Alice <fuzzything44@gmail.com>
2026-02-19 17:38:55 -0800

    More Damage Tracker updates Decently large change this time, now splits damage by enemy with a sane default view (equivalent to the previous only view). Also, if enemy has painshare it shows it!

--- RNSReloaded.DamageTracker/Mod.cs
+++ RNSReloaded.DamageTracker/Mod.cs
@@ -15,18 +15,24 @@ public unsafe class Mod : IMod {
 
     private IHook<ScriptDelegate>? damageHook;
     private IHook<ScriptDelegate>? newFightHook;
+    private IHook<ScriptDelegate>? addEnemyHook;
 
+    private const string DEFAULT_ENEMY = "Default";
     private struct DamageInfo {
         public long damage;
         public long count;
     }
 
-    private Dictionary<long, DamageInfo>[] damageAmounts = [
-        new Dictionary<long, DamageInfo>(),
-        new Dictionary<long, DamageInfo>(),
-        new Dictionary<long, DamageInfo>(),
-        new Dictionary<long, DamageInfo>()
+    // playerId -> normalizedEnemyId -> hbId -> damage info
+    private Dictionary<string, Dictionary<long, DamageInfo>>[] damageAmounts = [
+        new Dictionary<string, Dictionary<long, DamageInfo>>(),
+        new Dictionary<string, Dictionary<long, DamageInfo>>(),
+        new Dictionary<string, Dictionary<long, DamageInfo>>(),
+        new Dictionary<string, Dictionary<long, DamageInfo>>()
     ];
+
+    private Dictionary<long, string> enemyIdLookup = new Dictionary<long, string>();
+    private Dictionary<string, long> enemyNameUses = new Dictionary<string, long>();
 
     private Dictionary<long, string> hbIdNameOverrides = new Dictionary<long, string>() {
         { -1, "Debuffs" },
@@ -36,11 +42,45 @@ public unsafe class Mod : IMod {
         { 4, "Defensive" }
     };
 
+    private DamageInfo getDamageInfo(long playerId, long enemyId, long hbId) {
+        // PlayerID will always exist. Probably.
+        var playerDict = this.damageAmounts[playerId];
+        if (!playerDict.ContainsKey(this.enemyIdLookup.GetValueOrDefault(enemyId, "UNKNOWN_ENEMY"))) {
+            return new DamageInfo() { count = 0, damage = 0 };
+        }
+        var enemyDict = playerDict[this.enemyIdLookup.GetValueOrDefault(enemyId, "UNKNOWN_ENEMY")];
+        if (!enemyDict.ContainsKey(hbId)) {
+            return new DamageInfo() { count = 0, damage = 0 };
+        }
+        return enemyDict[hbId];
+    }
+
+    private Dictionary<long, DamageInfo> getHbDamageDict(long playerId, string enemyId) {
+        // PlayerID will always exist. Probably.
+        var playerDict = this.damageAmounts[playerId];
+        if (!playerDict.ContainsKey(enemyId)) {
+            return new Dictionary<long, DamageInfo>();
+        }
+        return playerDict[enemyId];
+    }
+
+    private void setDamageInfo(long playerId, long enemyId, long hbId, DamageInfo toSet) {
+        // PlayerID will always exist. Probably.
+        var playerDict = this.damageAmounts[playerId];
+        if (!playerDict.ContainsKey(this.enemyIdLookup.GetValueOrDefault(enemyId, "UNKNOWN_ENEMY"))) {
+            playerDict[this.enemyIdLookup.GetValueOrDefault(enemyId, "UNKNOWN_ENEMY")] = new Dictionary<long, DamageInfo>();
+        }
+        var enemyDict = playerDict[this.enemyIdLookup.GetValueOrDefault(enemyId, "UNKNOWN_ENEMY")];
+        enemyDict[hbId] = toSet;
+    }
+
     public void StartEx(IModLoaderV1 loader,IModConfigV1 modConfig) {
         this.rnsReloadedRef = loader.GetController<IRNSReloaded>();
         this.hooksRef = loader.GetController<IReloadedHooks>()!;
         
         this.logger = loader.GetLogger();
+
+        this.Reset();
 
         if (this.rnsReloadedRef.TryGetTarget(out var rnsReloaded)) {
             rnsReloaded.OnReady += this.Ready;
@@ -77,10 +117,29 @@ public unsafe class Mod : IMod {
                 hooks.CreateHook<ScriptDelegate>(this.NewFightDetour, newFightScript->Functions->Function);
             this.newFightHook.Activate();
             this.newFightHook.Enable();
+
+            var addEnemyScript = rnsReloaded.GetScriptData(rnsReloaded.ScriptFindId("scrdt_enemy") - 100000);
+            this.addEnemyHook =
+                hooks.CreateHook<ScriptDelegate>(this.AddEnemyDetour, addEnemyScript->Functions->Function);
+            this.addEnemyHook.Activate();
+            this.addEnemyHook.Enable();
+
+            // Player applying debuffs or buffs: (note: called even for buffs that already exist)
+            // args[0] = player/enemyID
+            // args[1] = always 1?
+            // args[2] = always undefined?
+            // returnValue = did it actually apply maybe?
+            // scrbp_add_hbs(0, 1, undefined) -> 1 
+
+
+            // Phase change, we want to split damage by boss phase (probably) because separate enrage timers
+            // scrbp_phase_pattern_remove
         }
     }
 
     private double? initialPainshare = null;
+    private string initialEnemy = "";
+
     private RValue* EnemyDamageDetour(
         CInstance* self, CInstance* other, RValue* returnValue, int argc, RValue** argv
     ) {
@@ -96,11 +155,16 @@ public unsafe class Mod : IMod {
                 var painShare = rnsReloaded.utils.RValueToDouble(rnsReloaded.FindValue(rnsReloaded.GetGlobalInstance(), "playerPainshareRatio")->Get(1)->Get(0));
                 this.initialPainshare = painShare;
             }
-            if (enemyId == 0 || this.initialPainshare == 0) {
-                var dmgInfo = this.damageAmounts[playerId].GetValueOrDefault(hbId, new DamageInfo() { count = 0, damage = 0 });
+            var dmgInfo = this.getDamageInfo(playerId, enemyId, hbId);
+            dmgInfo.count++;
+            dmgInfo.damage += damage;
+            this.setDamageInfo(playerId, enemyId, hbId, dmgInfo);
+
+            if (this.initialPainshare == 0 || this.enemyIdLookup[enemyId] == this.initialEnemy) {
+                dmgInfo = this.damageAmounts[playerId][DEFAULT_ENEMY].GetValueOrDefault(hbId, new DamageInfo() { count = 0, damage = 0});
                 dmgInfo.count++;
                 dmgInfo.damage += damage;
-                this.damageAmounts[playerId][hbId] = dmgInfo;
+                this.damageAmounts[playerId][DEFAULT_ENEMY][hbId] = dmgInfo;
             }
         }
 
@@ -108,18 +172,59 @@ public unsafe class Mod : IMod {
         return returnValue;
     }
 
+    private void Reset() {
+        this.initialPainshare = null;
+        this.initialEnemy = "";
+        // If they selected an enemy, then their selection will be invalid next fight
+        // So we set it back to default for a better UX
+        this.selectedEnemy = DEFAULT_ENEMY;
+
+        foreach (var dict in this.damageAmounts) {
+            dict.Clear();
+            // Always add a default option, which stores either damage to the main enemy (if painshare) or total damage (if not)
+            // Easier to update it here/when tracking damage than to do the math every display frame.
+            dict["Default"] = new Dictionary<long, DamageInfo>();
+        }
+
+        this.enemyIdLookup.Clear();
+        this.enemyNameUses.Clear();
+    }
+
     private RValue* NewFightDetour(
         CInstance* self, CInstance* other, RValue* returnValue, int argc, RValue** argv
     ) {
-        this.initialPainshare = null;
-        foreach (var dict in this.damageAmounts) {
-            dict.Clear();
-        }
+        this.Reset();
         returnValue = this.newFightHook!.OriginalFunction(self, other, returnValue, argc, argv);
         return returnValue;
     }
 
+    private void AddEnemy(string enemyName, long enemyListId) {
+        // This causes us to 1-index names, since it's displayed to the player.
+        // it's not really an "index" as we don't array off it, so it shouldn't cause off by 1 errors
+        var nameId = this.enemyNameUses.GetValueOrDefault(enemyName, 0) + 1;
+        this.enemyNameUses[enemyName] = nameId;
+        this.enemyIdLookup[enemyListId] = enemyName + " " + nameId;
+        if (this.initialEnemy == "") {
+            this.initialEnemy = enemyName + " " + nameId;
+        }
+    }
+
+    private RValue* AddEnemyDetour(
+        CInstance* self, CInstance* other, RValue* returnValue, int argc, RValue** argv
+    ) {
+        if (this.rnsReloadedRef!.TryGetTarget(out var rnsReloaded)) {
+            var enemyRealId = rnsReloaded.utils.RValueToLong(argv[0]);
+            // [key, name w/ title, name w/o title, more stuff]
+            var enemyName = rnsReloaded.FindValue(rnsReloaded.GetGlobalInstance(), "enemyData")->Get((int) enemyRealId)->Get(2)->ToString();
+            var enemyListId = rnsReloaded.utils.RValueToLong(rnsReloaded.FindValue(self, "playerId"));
+            this.AddEnemy(enemyName, enemyListId);
+        }
+        returnValue = this.addEnemyHook!.OriginalFunction(self, other, returnValue, argc, argv);
+        return returnValue;
+    }
+
     private int selectedPlayer = 0;
+    private string selectedEnemy = DEFAULT_ENEMY;
     public void Draw() {
         if (!this.ready) return;
         if (this.rnsReloadedRef!.TryGetTarget(out var rnsReloaded)) {
@@ -142,8 +247,30 @@ public unsafe class Mod : IMod {
                     }
                 }
 
+                if (ImGui.TreeNodeStr("Enemy Select")) {
+                    if (this.initialPainshare.HasValue && this.initialPainshare != 0) {
+                        ImGui.Text($"Initial Painshare: {this.initialPainshare * 100}%%");
+                    }
+
+                    int wrapIndex = 0;
+                    foreach (var item in this.damageAmounts[this.selectedPlayer].Keys) {
+
+                        if (wrapIndex % 3 != 0) {
+                            ImGui.SameLine(0, 10);
+                        }
+                        wrapIndex++;
+
+                        if (this.selectedEnemy == item) {
+                            ImGui.Text(item);
+                        } else if (ImGui.Button(item, buttonSize)) {
+                            this.selectedEnemy = item;
+                        }
+                    }
+                    ImGui.TreePop();
+                }
+
                 if (ImGui.BeginTable("", 4, 384, buttonSize, 0)) {
-                    var orderedKeys = this.damageAmounts[this.selectedPlayer].Keys.Order();
+                    var orderedKeys = this.getHbDamageDict(this.selectedPlayer, this.selectedEnemy).Keys.Order();
                     ImGui.TableNextRow(0, 0);
                     ImGui.TableNextColumn();
                     ImGui.TableHeader("Source");
@@ -156,9 +283,9 @@ public unsafe class Mod : IMod {
 
                     ImGui.TableNextColumn();
                     ImGui.TableHeader("% Total");
-                    long totalDamage = this.damageAmounts[this.selectedPlayer].Values.Sum(x => x.damage);
+                    long totalDamage = this.getHbDamageDict(this.selectedPlayer, this.selectedEnemy).Values.Sum(x => x.damage);
                     foreach (var key in orderedKeys) {
-                        // getting Potion #5 for 2nd player primary (debuffs still debuffs)
+                        // Adjust hBId for multiplayer, since each player gets 14 HBs
                         var adjustedKey = key;
                         if (adjustedKey > 0) {
                             adjustedKey -= this.selectedPlayer * 14;
@@ -182,17 +309,16 @@ public unsafe class Mod : IMod {
                                 hbName = "HB id " + adjustedKey;
                             }
                         }
-                        long damage = this.damageAmounts[this.selectedPlayer][key].damage;
-                        long hits = this.damageAmounts[this.selectedPlayer][key].count;
+                        var info = this.getHbDamageDict(this.selectedPlayer, this.selectedEnemy)[key];
                         ImGui.TableNextRow(0, 0);
                         ImGui.TableNextColumn();
                         ImGui.Text($"{hbName}");
                         ImGui.TableNextColumn();
-                        ImGui.Text($"{damage}");
+                        ImGui.Text($"{info.damage}");
                         ImGui.TableNextColumn();
-                        ImGui.Text($"{hits}");
+                        ImGui.Text($"{info.count}");
                         ImGui.TableNextColumn();
-                        ImGui.Text($"{Math.Round(100f * damage / totalDamage, 1)}%");
+                        ImGui.Text($"{Math.Round(100f * info.damage / totalDamage, 1)}%");
                     }
                     ImGui.EndTable();
                 }
--- RNSReloaded.DamageTracker/README.md
+++ RNSReloaded.DamageTracker/README.md
@@ -1,3 +1,7 @@
 # Damage tracker
 
 FFXIV ACT but bad
+
+Use the player buttons (labeled with their in-game names) to select which player you want to see damage stats on.
+
+Expand the "Enemy Select" dropdown to select which enemy you want to see damage stats on. The default is either just damage (including painshared damage) if the main enemy has painshare, otherwise it includes total damage to all enemies (for multi enemy fights)
